cde39d8f1e5cb4701add0d7753fdc9d8abdd5ab3
Luciano Santos <pdform@gmail.com>
2016-08-15 16:26:14 +0900

    * bfs search doc done

--- clue.pl
+++ clue.pl
@@ -1,22 +1,8 @@
 % Author: Luciano Santos
 % Date: 12/08/2016
-% This script plays Clue (Hasbro, 2002). See doc.pdf for reference.
+% This script plays Clue (Hasbro, 2002). See doc/doc.pdf for reference.
 
-
-%%%%% Auxiliary predicates.
-
-%% member(X, List) - X is in List
-member(X, [X|_]).
-member(X, [_|Tail]) :- member(X, Tail).
-
-%% append(L1, L2, L3) - L3 is the result of appending L1 and L2.
-append([], L, L).
-append([H|T], L2, [H|L3]) :- append(T, L2, L3).
-
-%% reverse(List, Reversed) - reverses List
-reverse_aux([H|Tail], Stack, Reversed) :- reverse_aux(Tail, [H|Stack], Reversed).
-reverse_aux([], Stack, Stack). 
-reverse(List, Reversed) :- reverse_aux(List, [], Reversed).
+:- use_module(library(lists)).
 
 %%%%% The static game data.
 
@@ -59,102 +45,89 @@ blocked((X, Y)) :- X >= 1, X =< 5, Y >= 8, Y =< 12.
 % stairs
 blocked((X, Y)) :- X >= 9, X =< 13, Y >= 10, Y =< 16.
 
-%% valid_room(R) - R is a valid room name
-valid_room(R) :-
-	member(
-		R,
-		[
-			'conservatory',
-			'ball room',
-			'kitchen',
-			'dining room',
-			'lounge',
-			'hall',
-			'study',
-			'library',
-			'billiard room'
-		]
-	).
+%% valid_rooms(Rooms) - Rooms is the list of valid room names.
+valid_rooms(
+	[
+		'conservatory',
+		'ball room',
+		'kitchen',
+		'dining room',
+		'lounge',
+		'hall',
+		'study',
+		'library',
+		'billiard room'
+	]
+).
 
-%% door((X, Y), R) - point <X, Y> is a door to room R
-door((4, 5), 'conservatory').
-door((8, 5), 'ball room').
-door((15, 5), 'ball room').
-door((9, 7), 'ball room').
-door((14, 7), 'ball room').
-door((19, 6), 'kitchen').
-door((16, 12), 'dining room').
-door((17, 15), 'dining room').
-door((17, 19), 'lounge').
-door((9, 20), 'hall').
-door((11, 18), 'hall').
-door((12, 18), 'hall').
-door((6, 21), 'study').
-door((3, 14), 'library').
-door((6, 16), 'library').
-door((1, 12), 'billiard room').
-door((5, 9), 'billiard room').
+%% door((X, Y), R) - there's a door to room R from point <X, Y>
+door((5, 5), 'conservatory').
+door((7, 5), 'ball room').
+door((16, 5), 'ball room').
+door((9, 8), 'ball room').
+door((14, 8), 'ball room').
+door((19, 7), 'kitchen').
+door((15, 12), 'dining room').
+door((17, 16), 'dining room').
+door((17, 18), 'lounge').
+door((8, 20), 'hall').
+door((11, 17), 'hall').
+door((12, 17), 'hall').
+door((6, 20), 'study').
+door((3, 13), 'library').
+door((7, 16), 'library').
+door((1, 13), 'billiard room').
+door((6, 9), 'billiard room').
 
-%% passage(R1, R2) - there a passage from R1 to R2
+%% passage(Src, Dst) - there's a passage from Src to Dst
 passage_aux('conservatory', 'lounge').
 passage_aux('kitchen', 'study').
-passage(R1, R2) :- passage_aux(R1, R2) ; passage_aux(R2, R1).
-
+passage(Src, Dst) :- passage_aux(Src, Dst) ; passage_aux(Dst, Src).
 
 %%% The game characters.
 
-%% valid_char(C) - is C a valid character name?
-valid_char(C) :-
-	member(
-		C,
-		[
-			'peacock',
-			'plum',
-			'green',
-			'white',
-			'scarlet',
-			'mustard'
-		]
-	).
-
-%%% The game weapons.
-
-%% valid_weapon(W) - is W a valid weapon name?
-valid_weapon(W) :-
-	member(
-		W,
-		[
-			'rope',
-			'pipe',
-			'knife',
-			'wrench',
-			'candlestick',
-			'pistol'
-		]
-	).
-
-
-
-%%%%% Movement logic.
-
-%%% The position of each character.
+%% valid_chars(Chars) - Chars is the list of valid characters,
+%% in the order they must play.
+valid_chars(
+	[
+		'scarlet',
+		'mustard',
+		'white',
+		'green',
+		'peacock',
+		'plum'
+	]
+).
 
 %% position((X, Y), C) - the current position of character C is <X, Y>
 %% here, it's initialized to the start position at the board.
-%% this predicate will be "rewritten" whenever the script's own character moves
-%% or it receives information that another character moved.
-position((0, 6), 'peacock').
-position((0, 19), 'plum').
-position((9, 0), 'green').
-position((14, 0), 'white').
+%% this predicate will be "rewritten" whenever the script's own
+%% character moves or it receives information that another character
+%% moved.
+:- dynamic position/2.
 position((16, 24), 'scarlet').
 position((23, 17), 'mustard').
+position((14, 0), 'white').
+position((9, 0), 'green').
+position((0, 6), 'peacock').
+position((0, 19), 'plum').
 
-%% is_free((X, Y)) - the position <X, Y> can bee occupied by a character.
-is_free((X, Y)) :-
-		X >= 0, X =< 23, Y >= 0, Y =< 23, % inside the boundaries of the board
-		\+ position((X, Y), _), % not currently occupied by anyone
-		\+ blocked((X, Y)). % not a room or a wall
+%%% The game weapons.
+
+%% valid_weapons(Weapons) - Weapons is the list of valid weapons.
+valid_weapons(
+	[
+		'rope',
+		'pipe',
+		'knife',
+		'wrench',
+		'candlestick',
+		'pistol'
+	]
+).
+
+
+%%%%% Movement logic.
 
 %% neighbor((Xs, Ys), (Xn, Yn)) - <Xn, Yn> is neighbor of <Xs, Ys>.
 neighbor((Xs, Ys), (Right, Ys)) :- Right is Xs + 1.
@@ -162,22 +135,28 @@ neighbor((Xs, Ys), (Left, Ys)) :- Left is Xs - 1.
 neighbor((Xs, Ys), (Xs, Up)) :- Up is Ys + 1.
 neighbor((Xs, Ys), (Xs, Down)) :- Down is Ys - 1.
 
+%% is_free((X, Y)) - the position <X, Y> can bee occupied by a character.
+is_free((X, Y)) :-
+		X >= 0, X =< 23, Y >= 0, Y =< 24, % inside the board
+		\+ position((X, Y), _), % not currently occupied by anyone
+		\+ blocked((X, Y)). % not a room or a wall
+
 %% closest_door((Xs, Ys), Room, Path) - starting at <Xs, Ys>, finds
 %% the closest Room door and the Path to it
-can_move_to((X, Y)) :- door((X, Y), _); is_free((X, Y)).
-valid_adjacent((X, Y), (Xd, Yd)) :-
+valid_adjacent((X, Y), (Xd, Yd), Seen) :-
 		neighbor((X, Y), (Xd, Yd)),
-		can_move_to((Xd, Yd)).
+		\+ member((Xd, Yd), Seen),
+		is_free((Xd, Yd)).
 closest_door_aux(Room, [(X, Y)], [(X, Y)|_], _) :- door((X, Y), Room).
 closest_door_aux(Room, [(X, Y)|PTail], [(X, Y)|QTail], Seen) :-
 		% print((X, Y)),
 		% print(QTail),nl,
 		% enqueues all non-seen adjacents to which it's possible to move
-		findall((Xd, Yd), (valid_adjacent((X, Y), (Xd, Yd)), \+ member((Xd, Yd), Seen)), ValidAdjacents),
+		findall((Xd, Yd), valid_adjacent((X, Y), (Xd, Yd), Seen), ValidAdjacents),
 		append(QTail, ValidAdjacents, Queue),
-		% considers all adjacent nodes as seen, to cut the search recursion
-		findall((Xa, Ya), neighbor((X, Y), (Xa, Ya)), Adjacents),
-		append(Adjacents, Seen, NewSeen),
+		% stores all adjacent nodes as seen, to cut the search recursion
+		findall((Xa, Ya), neighbor((X, Y), (Xa, Ya)), Neighbors),
+		union(Neighbors, Seen, NewSeen),
 		closest_door_aux(Room, PTail, Queue, NewSeen). % recursive definition
 closest_door((Xs, Ys), Room, Path) :-
 		is_free((Xs, Ys)),
